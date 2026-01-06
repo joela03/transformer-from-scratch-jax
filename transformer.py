072ea510fc8a9bc9302a583f113fe4df68ee5311
joela03 <joelallencaliste@gmail.com>
2026-01-06 23:58:49 +0000

    finished multi head attention implementatio

--- transformer.py
+++ transformer.py
@@ -2,6 +2,7 @@
 
 import jax
 import jax.numpy as jnp
+import haiku as hk
 
 def scaled_dot_product_attention(q, k, v, mask=None):
     """
@@ -30,4 +31,61 @@ def scaled_dot_product_attention(q, k, v, mask=None):
     output = jnp.matmul(attention_weights, v)
 
     return output
+
+class MultiHeadAttention(hk.module):
+    """Multi-head attention module."""
+
+    def __init__(
+        self,
+        num_heads:int,
+        d_model: int,
+        name: Optional[str]
+    ):
+        """
+        Args:
+            num_heads: Number of attention heads
+            d_model: Model dimension (must be divisible by num_heads)
+        """
+        super().__init__(name=name)
+        assert d_model % num_heads == 0, "d_model must be divisible by num_heads"
+        
+        self.num_heads = num_heads
+        self.d_model = d_model
+        self.d_k = d_model // num_heads
     
+    def __call__(self, x: jnp.ndarray) -> jnp.ndarray:
+        """
+        Args:
+            x: Input (batch, sew_len, d_model)
+
+        Returns:
+            Output (batch, seq_len, d_model)
+        """
+
+        batch_size = x.shape[0]
+        seq_len = x.shape[1]
+
+        # Initialise weight matrices
+        W_Q = hk.Linear(self.d_model)(x)
+        W_K = hk.Linear(self.d_model)(x)
+        W_V = hk.Linear(self.d_model)(x)
+
+        # Split into multiple heads
+        W_Q = W_Q.reshape(batch_size, seq_len, self.num_heads, self.d_k )
+        W_K = W_K.reshape(batch_size, seq_len, self.num_heads, self.d_k )
+        W_V = W_V.reshape(batch_size, seq_len, self.num_heads, self.d_k )
+
+        W_Q = W_Q.transpose(0, 2, 1, 3)
+        W_K = W_K.transpose(0, 2, 1, 3)
+        W_V = W_V.transpose(0, 2, 1, 3)
+
+        # Apply attention
+        attn_output = scaled_dot_product_attention(W_Q, W_K, W_V)
+        
+        # Concatenate heads
+        attn_output = attn_output.reshape(batch_size, seq_len, self.d_model)
+
+        # Multiply by final weight matrix
+        output = hk.Linear(self.d_model, name='output')(attn_output)
+
+        return output
